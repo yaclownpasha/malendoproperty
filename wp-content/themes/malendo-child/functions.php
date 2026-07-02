af791c8bf012d0f09fd8c1344f9266d5d6897a4b
htemal <pashapuzik08@gmail.com>
2026-07-02 22:45:41 +0700

    Conditionally dequeue WooCommerce assets on non-Woo pages

--- wp-content/themes/malendo-child/functions.php
+++ wp-content/themes/malendo-child/functions.php
@@ -18,6 +18,78 @@ add_action('wp_enqueue_scripts', function () {
     );
 }, 20);
 
+function malendo_dequeue_woocommerce_assets_on_non_woo_pages() {
+    if (
+        is_admin() ||
+        (function_exists('wp_doing_ajax') && wp_doing_ajax()) ||
+        (defined('REST_REQUEST') && REST_REQUEST) ||
+        (defined('WP_CLI') && WP_CLI) ||
+        (function_exists('wp_is_json_request') && wp_is_json_request())
+    ) {
+        return;
+    }
+
+    if (!function_exists('is_woocommerce')) {
+        return;
+    }
+
+    $woocommerce_page_checks = [
+        'is_woocommerce',
+        'is_cart',
+        'is_checkout',
+        'is_account_page',
+        'is_product',
+        'is_product_category',
+        'is_product_tag',
+    ];
+
+    foreach ($woocommerce_page_checks as $woocommerce_page_check) {
+        if (function_exists($woocommerce_page_check) && $woocommerce_page_check()) {
+            return;
+        }
+    }
+
+    if (function_exists('is_tax') && is_tax(['product_cat', 'product_tag'])) {
+        return;
+    }
+
+    // Performance safety patch: remove this block to rollback. It only strips WooCommerce frontend assets on clearly non-WooCommerce pages.
+    $woocommerce_style_handles = [
+        'woocommerce-general',
+        'woocommerce-layout',
+        'woocommerce-smallscreen',
+        'woocommerce-inline',
+        'wc-blocks-style',
+        'wc-blocks-vendors-style',
+        'wc-blocks-packages-style',
+        'wc-blocks-style-all-products',
+        'wc-blocks-style-all-reviews',
+    ];
+
+    $woocommerce_script_handles = [
+        'woocommerce',
+        'wc-add-to-cart',
+        'wc-cart-fragments',
+        'wc-add-to-cart-variation',
+        'js-cookie',
+        'wc-js-cookie',
+        'wc-jquery-blockui',
+        'sourcebuster-js',
+        'wc-order-attribution',
+    ];
+
+    foreach ($woocommerce_style_handles as $woocommerce_style_handle) {
+        wp_dequeue_style($woocommerce_style_handle);
+        wp_deregister_style($woocommerce_style_handle);
+    }
+
+    foreach ($woocommerce_script_handles as $woocommerce_script_handle) {
+        wp_dequeue_script($woocommerce_script_handle);
+        wp_deregister_script($woocommerce_script_handle);
+    }
+}
+add_action('wp_enqueue_scripts', 'malendo_dequeue_woocommerce_assets_on_non_woo_pages', 99);
+
 add_action('wp_head', function () {
     ?>
     <!-- Google tag (gtag.js) -->
